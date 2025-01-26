c0522b6db862dd34b82668d47a04897785d50044
Tommy <naviboun@gmail.com>
2025-01-25 21:26:11 -0600

    Final 3

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -1,3 +1,11 @@
+/**
+ * By Navi Bountho
+ * The class RemoveElement contains a method called removeElement
+ * that takes and integer array and int value as a inputs and returns
+ * the amount of integers that are not the target value.
+ * The method also moves all integers that are not equal to the target val
+ * towards the right of the array.
+ */
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
 
